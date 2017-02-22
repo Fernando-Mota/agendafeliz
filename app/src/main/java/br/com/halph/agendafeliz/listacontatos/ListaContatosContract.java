c9fa14e805fd239dce53586f52c72a74e1528ae2
Fernando Mota <fm.aken@gmail.com>
2017-02-21 22:28:48 -0300

    Correção no botão flutuante

--- app/src/main/java/br/com/halph/agendafeliz/listacontatos/ListaContatosContract.java
+++ app/src/main/java/br/com/halph/agendafeliz/listacontatos/ListaContatosContract.java
@@ -22,6 +22,6 @@ public interface ListaContatosContract {
 
         public void adicionaContatoClick();
 
-        public void alteraContatoClick();
+        public void alteraContatoClick(Contato contato);
     }
 }
--- app/src/main/java/br/com/halph/agendafeliz/listacontatos/ListaContatosFragment.java
+++ app/src/main/java/br/com/halph/agendafeliz/listacontatos/ListaContatosFragment.java
@@ -2,13 +2,17 @@ package br.com.halph.agendafeliz.listacontatos;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import br.com.halph.agendafeliz.R;
+import br.com.halph.agendafeliz.contatos.data.Contato;
+import br.com.halph.agendafeliz.formulariocontatos.FormularioContatoFragment;
 
 /**
  * Created by Android on 20/02/2017.
@@ -16,9 +20,11 @@ import br.com.halph.agendafeliz.R;
 
 public class ListaContatosFragment extends Fragment implements ListaContatosContract.View {
 
-    ListaContatosContract.Presenter presenter;
+    private ListaContatosContract.Presenter presenter;
 
-    Context context;
+    private Context context;
+
+    private FloatingActionButton botaoAdicionaContato;
 
 
     @Override
@@ -26,6 +32,16 @@ public class ListaContatosFragment extends Fragment implements ListaContatosCont
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.lista_contato_fragment, container, false);
+
+
+        botaoAdicionaContato = (FloatingActionButton) view.findViewById(R.id.adiciona_contatos);
+
+        botaoAdicionaContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adicionaContatoClick();
+            }
+        });
 
         // Inflate the layout for this fragment
         return view;
@@ -43,10 +59,22 @@ public class ListaContatosFragment extends Fragment implements ListaContatosCont
     @Override
     public void adicionaContatoClick() {
 
+        // Create new fragment and transaction
+        Fragment newFragment = new FormularioContatoFragment();
+        // consider using Java coding conventions (upper first char class names!!!)
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack
+        transaction.replace(R.id.dash_board_container, newFragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
     }
 
     @Override
-    public void alteraContatoClick() {
+    public void alteraContatoClick(Contato contato) {
 
     }
 
